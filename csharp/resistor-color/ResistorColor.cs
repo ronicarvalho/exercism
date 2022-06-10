fb3150511f092163ae841e6d1acd629609f071b6
Roni Carvalho <roni.carvalho@outlook.com>
2022-06-10 19:18:37 -0300

    feat: Resistor Color

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -2,13 +2,15 @@
 
 public static class ResistorColor
 {
+    private static string[] mnemonics = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
     public static int ColorCode(string color)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Array.IndexOf(mnemonics, color);
     }
 
     public static string[] Colors()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return mnemonics;
     }
 }
