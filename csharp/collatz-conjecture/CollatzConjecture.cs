a6a77bdf82538b7b2c4a336b135f8decf2ff7cf3
Roni Carvalho <roni.carvalho@outlook.com>
2022-06-10 20:38:26 -0300

    feat: Collatz Conjecture

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -4,6 +4,14 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        if (number <= 0) throw new ArgumentOutOfRangeException();
+        if (number == 1) return 0;
+
+        var steps = 1;
+
+        while ((number = ((number ^ 1) == number + 1) ? (number / 2) : ((number * 3) + 1)) != 1) 
+            steps++;
+
+        return steps;
     }
 }
